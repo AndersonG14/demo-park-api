9db915e7940a1f4d84e30c7d0d377c84f5e79256
Anderson Gomes <gomedanderson@gmail.com>
2025-09-19 20:34:33 -0300

    adicionando funcionalidades na classe UsuarioController

--- demo-park-api/src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
+++ demo-park-api/src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
@@ -4,10 +4,16 @@ import com.mballem.demo_park_api.entity.Usuario;
 import com.mballem.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
