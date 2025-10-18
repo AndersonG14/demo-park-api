5e970ca2a19c142436a32edd8cf20f33bf028eb7
Anderson Gomes <gomedanderson@gmail.com>
2025-10-18 10:49:36 -0300

    configurando o ambiente de testes

---
+++ demo-park-api/src/test/java/com/mballem/demo_park_api/UsuarioIT.java
@@ -0,0 +1,12 @@
+package com.mballem.demo_park_api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UsuarioIT {
+
+    @Autowired
+    WebTestClient testClient;
+}
