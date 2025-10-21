59ac789c82f24328cfb9f8d4e1a83552581b1a90
Anderson Gomes <gomedanderson@gmail.com>
2025-10-21 14:28:18 -0300

    criando a classe SpringSecurityConfig

--- demo-park-api/src/main/java/com/mballem/demo_park_api/config/SpringSecurityConfig.java
+++ demo-park-api/src/main/java/com/mballem/demo_park_api/config/SpringSecurityConfig.java
@@ -3,8 +3,10 @@ package com.mballem.demo_park_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -21,9 +23,12 @@ public class SpringSecurityConfig {
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers()
+                        .requestMatchers(HttpMethod.POST, "api/v1/usuarios").permitAll()
+                        .anyRequest().authenticated()
 
-                )
+                ).sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).build();
     }
 
 }
