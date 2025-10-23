d7290a6400d10eb28291b84b725132d265089495
Anderson Gomes <gomedanderson@gmail.com>
2025-10-23 09:52:22 -0300

    criando a classe JwtUserDetailsService

--- demo-park-api/src/main/java/com/mballem/demo_park_api/repository/UsuarioRepository.java
+++ demo-park-api/src/main/java/com/mballem/demo_park_api/repository/UsuarioRepository.java
@@ -3,5 +3,8 @@ package com.mballem.demo_park_api.repository;
 import com.mballem.demo_park_api.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
 }
