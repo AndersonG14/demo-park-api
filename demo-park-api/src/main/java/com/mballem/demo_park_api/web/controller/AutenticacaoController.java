d5117c5bfc6b81f5ada2f79d2ebc000c70b7cad7
Anderson Gomes <gomedanderson@gmail.com>
2025-10-23 10:39:04 -0300

    configurando o controller de Autenticacao

--- demo-park-api/src/main/java/com/mballem/demo_park_api/web/controller/AutenticacaoController.java
+++ demo-park-api/src/main/java/com/mballem/demo_park_api/web/controller/AutenticacaoController.java
@@ -3,12 +3,16 @@ package com.mballem.demo_park_api.web.controller;
 import com.mballem.demo_park_api.jwt.JwtToken;
 import com.mballem.demo_park_api.jwt.JwtUserDetailsService;
 import com.mballem.demo_park_api.web.dto.mapper.UsuarioLoginDto;
+import com.mballem.demo_park_api.web.exception.ErrorMessage;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +27,19 @@ public class AutenticacaoController {
     private final JwtUserDetailsService detailsService;
     private final AuthenticationManager authenticationManager;
 
-    public ResponseEntity<JwtToken> autenticar(@RequestBody @Valid UsuarioLoginDto dto, HttpServletRequest request){
-        log.info("Processo de autenticação pelo login {}", dto.getUsername());
+    public ResponseEntity<?>autenticar(@RequestBody @Valid UsuarioLoginDto dto, HttpServletRequest request){
+        log.info("Processo de autenticação pelo login '{}'", dto.getUsername());
         try {
-
-        } catch ()
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword());
+            authenticationManager.authenticate(authenticationToken);
+            JwtToken token = detailsService.getTokenAuthenticated(dto.getUsername());
+            return ResponseEntity.ok(token);
+        } catch (AuthenticationException ex) {
+            log.warn("Bad Crendentials from username '{}'", dto.getUsername());
+        }
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Credenciais inválidas"));
     }
 }
