17a27733ffa5c894e540088e8d5132a82253be22
Anderson Gomes <gomedanderson@gmail.com>
2025-10-24 09:31:47 -0300

    ajustes para o processo de autenticação e login

--- demo-park-api/src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
+++ demo-park-api/src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
@@ -7,6 +7,7 @@ import com.mballem.demo_park_api.exception.UsernameUniqueViolationException;
 import com.mballem.demo_park_api.jwt.JwtUtils;
 import com.mballem.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,10 +18,12 @@ import java.util.List;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
         try {
+            usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
             return usuarioRepository.save(usuario);
         } catch (org.springframework.dao.DataIntegrityViolationException ex) {
             throw new UsernameUniqueViolationException(String.format("Username {%s} já cadastrado", usuario.getUsername()));
@@ -42,11 +45,11 @@ public class UsuarioService {
         }
 
         Usuario user = buscarPorId(id);
-        if(!user.getPassword().equals(senhaAtual)){
+        if(!passwordEncoder.matches(senhaAtual, user.getPassword())){
             throw new PasswordInvalidException("Sua senha não confere.");
         }
 
-        user.setPassword(novaSenha);
+        user.setPassword(passwordEncoder.encode(novaSenha));
         return user;
     }
 
