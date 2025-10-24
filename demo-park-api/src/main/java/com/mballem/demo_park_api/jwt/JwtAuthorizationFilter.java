f8a828da780260cc202984b7026e64df46e687f9
Anderson Gomes <gomedanderson@gmail.com>
2025-10-24 09:12:25 -0300

    criando um filtro de interceptação de requisição do cliente

--- demo-park-api/src/main/java/com/mballem/demo_park_api/jwt/JwtAuthorizationFilter.java
+++ demo-park-api/src/main/java/com/mballem/demo_park_api/jwt/JwtAuthorizationFilter.java
@@ -6,6 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -28,6 +33,26 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             return;
         }
 
+        if (JwtUtils.isTokenValid(token)) {
+            log.warn("JWT está inválido ou expirado.");
+            filterChain.doFilter(request, response);
+            return;
+        }
 
+        String username = JwtUtils.getUsernameFromToken(token);
+
+        toAuthentication(request, username);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void toAuthentication(HttpServletRequest request, String username) {
+        UserDetails userDetails = detailsService.loadUserByUsername(username);
+
+        UsernamePasswordAuthenticationToken authenticationToken =  UsernamePasswordAuthenticationToken
+                .authenticated(userDetails, null, userDetails.getAuthorities());
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
     }
 }
