6eaf9b3ca9d199da588408c6c9a09dce1f705903
Anderson Gomes <gomedanderson@gmail.com>
2025-10-24 22:14:58 -0300

    ajustes para o processo de autenticação e login

--- demo-park-api/src/main/java/com/mballem/demo_park_api/jwt/JwtAuthorizationFilter.java
+++ demo-park-api/src/main/java/com/mballem/demo_park_api/jwt/JwtAuthorizationFilter.java
@@ -27,13 +27,13 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         final String token = request.getHeader(JwtUtils.JWT_AUTHORIZATION);
-        if (token == null || token.startsWith(JwtUtils.JWT_BEARER)) {
+        if (token == null || !token.startsWith(JwtUtils.JWT_BEARER)) {
             log.info("JWT está nulo, vazio ou não iniciado com 'Bearer '.");
             filterChain.doFilter(request, response);
             return;
         }
 
-        if (JwtUtils.isTokenValid(token)) {
+        if (!JwtUtils.isTokenValid(token)) {
             log.warn("JWT está inválido ou expirado.");
             filterChain.doFilter(request, response);
             return;
