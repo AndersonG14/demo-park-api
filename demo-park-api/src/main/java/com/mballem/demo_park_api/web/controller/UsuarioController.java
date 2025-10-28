88bad9879b86be9537b48723bc779b99f37677a5
Anderson Gomes <gomedanderson@gmail.com>
2025-10-28 09:34:10 -0300

    permissão de acesso para administrador ao recurso de localização pelo id

--- demo-park-api/src/main/java/com/mballem/demo_park_api/web/controller/UsuarioController.java
+++ demo-park-api/src/main/java/com/mballem/demo_park_api/web/controller/UsuarioController.java
@@ -17,6 +17,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -56,6 +57,8 @@ public class UsuarioController {
             }
 
     )
+
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id){
         Usuario user = usuarioService.buscarPorId(id);
