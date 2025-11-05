3ca2e333985686494eaee798692fbbb51090f726
Anderson Gomes <gomedanderson@gmail.com>
2025-11-04 22:23:53 -0300

    Repostiry e Service para Cliente

---
+++ demo-park-api/src/main/java/com/mballem/demo_park_api/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package com.mballem.demo_park_api.repository;
+
+import com.mballem.demo_park_api.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+}
