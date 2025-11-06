fe9b1a3cb557b8a083f0435fe01c4f3004713f1e
Anderson Gomes <gomedanderson@gmail.com>
2025-11-06 09:54:17 -0300

    teste de ponto a ponto para Create (Status 201)

--- demo-park-api/src/main/java/com/mballem/demo_park_api/web/controller/ClienteController.java
+++ demo-park-api/src/main/java/com/mballem/demo_park_api/web/controller/ClienteController.java
@@ -13,6 +13,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name="Cliente", description = "Contém todas as operações relativas ao recurso de um cliente")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/clientes")
