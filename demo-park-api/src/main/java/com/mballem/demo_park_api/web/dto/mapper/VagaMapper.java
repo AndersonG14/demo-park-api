55738184b9896edc018f91a27748980ed2f461bd
Anderson Gomes <gomedanderson@gmail.com>
2025-11-21 10:00:37 -0300

    add os DTOs e Mapper para Vagas

---
+++ demo-park-api/src/main/java/com/mballem/demo_park_api/web/dto/mapper/VagaMapper.java
@@ -0,0 +1,20 @@
+package com.mballem.demo_park_api.web.dto.mapper;
+
+import com.mballem.demo_park_api.entity.Vaga;
+import com.mballem.demo_park_api.web.dto.VagaResponseDto;
+import com.mballem.demo_park_api.web.dto.VagasCreateDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class VagaMapper {
+
+    public static Vaga toVaga(VagasCreateDto dto) {
+        return new ModelMapper().map(dto, Vaga.class);
+    }
+
+    public static VagaResponseDto toDto(Vaga vaga) {
+        return new ModelMapper().map(vaga, VagaResponseDto.class);
+    }
+}
